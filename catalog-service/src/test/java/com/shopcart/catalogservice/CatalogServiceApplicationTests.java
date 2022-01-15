b0f65306b9ab48e837b5adee228320081f9624ad
Reuben Ninan <intelphysic@gmail.com>
2022-01-15 15:50:56 -0500

    catalog spring service init

---
+++ catalog-service/src/test/java/com/shopcart/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopcart.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
