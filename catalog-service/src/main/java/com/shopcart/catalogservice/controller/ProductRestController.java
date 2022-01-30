3accf39fad454e4bc8f853d83f5738eb7e6a840b
Reuben Ninan <intelphysic@gmail.com>
2022-01-29 21:06:28 -0500

    added post route for products

--- catalog-service/src/main/java/com/shopcart/catalogservice/controller/ProductRestController.java
+++ catalog-service/src/main/java/com/shopcart/catalogservice/controller/ProductRestController.java
@@ -3,9 +3,7 @@ package com.shopcart.catalogservice.controller;
 import com.shopcart.catalogservice.entity.Product;
 import com.shopcart.catalogservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,11 @@ public class ProductRestController {
     @GetMapping
     public List<Product> getAllProducts() {
         return (List<Product>) productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productRepository.save(product);
     }
 
 }
