36c44635275520a26328702c48195121a9e297dd
Reuben <intelphysic@gmail.com>
2022-03-17 16:35:04 -0400

    changed endpoints to return the mutated object

--- shopcart-backend/src/main/java/com/shopcart/backend/controller/ReviewRestController.java
+++ shopcart-backend/src/main/java/com/shopcart/backend/controller/ReviewRestController.java
@@ -33,23 +33,26 @@ public class ReviewRestController {
     }
 
     @PostMapping("/{product_id}")
-    public void addReview(@RequestBody Review review, @PathVariable long product_id) {
+    public Review addReview(@RequestBody Review review, @PathVariable long product_id) {
     	reviewRepository.save(review);
         Product product = productRepository.findById(product_id).orElse(null);
         product.addReview_id(review.getId());
         productRepository.save(product);
+        return review;
     }
 
     @PutMapping("/{review_id}")
-    public void updateReview(@RequestBody Review review, @PathVariable long review_id) {
+    public Review updateReview(@RequestBody Review review, @PathVariable long review_id) {
     	if(review_id == review.getId()) reviewRepository.save(review);
+        return review;
     }
 
     @DeleteMapping("/{review_id}")
-    public void deleteReview(@RequestBody Review review, @PathVariable long review_id) {
+    public Review deleteReview(@RequestBody Review review, @PathVariable long review_id) {
     	reviewRepository.delete(review);
         Product product = productRepository.findById(review_id).orElse(null);
         product.removeReview_id(review.getId());
         productRepository.save(product);
+        return review;
     }
 }
