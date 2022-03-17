a0303e1a0bf9058875b83b7d7948c00dd32cc97a
Reuben <intelphysic@gmail.com>
2022-03-17 16:30:07 -0400

    added CORS global config

--- shopcart-backend/src/main/java/com/shopcart/backend/ShopCartBackendApplication.java
+++ shopcart-backend/src/main/java/com/shopcart/backend/ShopCartBackendApplication.java
@@ -2,8 +2,9 @@ package com.shopcart.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ShopCartBackendApplication {
 
 	public static void main(String[] args) {
