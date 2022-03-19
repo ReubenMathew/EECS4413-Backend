33b9fcdb9f752a38a4efbbee998afbbf72f4cb90
Reuben <intelphysic@gmail.com>
2022-03-19 15:21:07 -0400

    disable csrf protection for debuggin

--- shopcart-backend/src/main/java/com/shopcart/backend/config/SecurityConfig.java
+++ shopcart-backend/src/main/java/com/shopcart/backend/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/api/register","/api/authenticate","/api/products").permitAll().anyRequest()
+        http.csrf().disable().authorizeRequests().antMatchers("/api/register","/api/authenticate","/api/products").permitAll().anyRequest()
                 .authenticated().and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
