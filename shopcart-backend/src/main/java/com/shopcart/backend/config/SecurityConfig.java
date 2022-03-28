109950e21bdf950651bb5105d67e54a4058c7178
Reuben <intelphysic@gmail.com>
2022-03-27 21:39:31 -0400

    cors config for security adapter

--- shopcart-backend/src/main/java/com/shopcart/backend/config/SecurityConfig.java
+++ shopcart-backend/src/main/java/com/shopcart/backend/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .cors().and()
             .csrf().disable()
             .authorizeRequests()
                 .antMatchers("/api/register", "/api/authenticate").permitAll()
